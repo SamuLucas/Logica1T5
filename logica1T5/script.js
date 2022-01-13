328784a8abd1c87778f1b9b2637364d94a58919a
SamuLucas <samulucasf@hotmail.com>
2022-01-12 21:55:25 -0300

    logica1T5

---
+++ logica1T5/script.js
@@ -0,0 +1,11 @@
+//exibe uma mensagem no console do navegador//
+console.log("Hello world");
+let nome = "Samuel";
+let numero = 100;
+let anoAtual = 2022 - numero
+console.log(anoAtual - numero)
+console.log(anoAtual + numero)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
